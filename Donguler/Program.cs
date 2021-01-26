f6cd249b8729e50bac68baf6ee2ab006ad0987b0
NizesLore <fenciszn@hotmail.com>
2021-01-26 18:59:25 +0300

    16 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangış için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "C#";
+
+            string [] kurslar = new string [] {"Yazılım Geliştirici Yetiştirme Kampı",
+            "Programlamaya başlangış için temel kurs","Java","C#"};
+
+                   
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar [i]);
+            }
+            Console.WriteLine("for bitti);
+
+            foreach (kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            
+            Console.WriteLine("Sayfa sonu-footer");
+        }
+    }
+
+}        
+
+
+
---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+            
+            
+            
+            Console.WriteLine(kategoriEtiketi);
+            
+
+        }
+    }
+}
+
